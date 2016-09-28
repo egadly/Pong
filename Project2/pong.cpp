ad80c44f58f54a0030513540b4106c00bf8dec0c
egadly <ernest.gaddi.491@my.csun.edu>
2016-09-28 15:49:24 -0700

    Colors0.2

--- Project2/pong.cpp
+++ Project2/pong.cpp
@@ -13,7 +13,7 @@ P1::P1() {
 	pos.x = 24;
 	pos.y = 0;
 	shape = sf::RectangleShape(sf::Vector2f(16, 32));
-	shape.setFillColor(sf::Color::White);
+	shape.setFillColor(sf::Color::Red);
 }
 void P1::update(sf::Keyboard keyboard) {
 	if (keyboard.isKeyPressed(sf::Keyboard::W)) pos.y -= 4;
@@ -26,7 +26,7 @@ P2::P2() {
 	pos.x = 320 - 40;
 	pos.y = 240 - 32;
 	shape = sf::RectangleShape(sf::Vector2f(16, 32));
-	shape.setFillColor(sf::Color::White);
+	shape.setFillColor(sf::Color::Blue);
 }
 void P2::update(sf::Keyboard keyboard) {
 	if (keyboard.isKeyPressed(sf::Keyboard::I)) pos.y -= 4;
@@ -41,7 +41,7 @@ Ball::Ball() {
 	vel.x = 4;
 	vel.y = 4;
 	shape = sf::RectangleShape(sf::Vector2f(16, 16));
-	shape.setFillColor(sf::Color::White);
+	shape.setFillColor(sf::Color::Red);
 	font.loadFromFile("num.ttf");
 	text.setFont(font);
 	text.setCharacterSize(16);
@@ -53,18 +53,26 @@ void Ball::update(P1 player1, P2 player2) {
 	if (vel.x < -16) vel.x = -16;
 	pos.x += vel.x;
 	pos.y += vel.y;
-	if (vel.x < 0 && (pos.x - player1.getPos().x)< 16 && (pos.x - player1.getPos().x)>-16 && (pos.y - player1.getPos().y) < 32 && (pos.y - player1.getPos().y) > -16) vel.x = -vel.x*1.1;
-	if (vel.x > 0 && (pos.x - player2.getPos().x)< 16 && (pos.x - player2.getPos().x)>-16 && (pos.y - player2.getPos().y) < 32 && (pos.y - player2.getPos().y) > -16) vel.x = -vel.x*1.1;
+	if (vel.x < 0 && (pos.x - player1.getPos().x) < 16 && (pos.x - player1.getPos().x) > -16 && (pos.y - player1.getPos().y) < 32 && (pos.y - player1.getPos().y) > -16) {
+		vel.x = -vel.x*1.1;
+		shape.setFillColor(sf::Color::Red);
+	}
+	if (vel.x > 0 && (pos.x - player2.getPos().x) < 16 && (pos.x - player2.getPos().x) > -16 && (pos.y - player2.getPos().y) < 32 && (pos.y - player2.getPos().y) > -16) {
+		vel.x = -vel.x*1.1;
+		shape.setFillColor(sf::Color::Blue);
+	}
 
 	if (pos.x < 0 ) {
 		pos.x = 320 / 2;
 		vel.x = 4;
 		score.y += 1;
+		shape.setFillColor(sf::Color::Red);
 	}
 	if (pos.x > 320-16) {
 		pos.x = 320 / 2;
 		vel.x = -4;
 		score.x += 1;
+		shape.setFillColor(sf::Color::Blue);
 	}
 	if (pos.y < 0) {
 		pos.y = 0;
@@ -80,9 +88,11 @@ void Ball::update(P1 player1, P2 player2) {
 void Ball::draw(sf::RenderWindow * window) {
 	text.setString(std::to_string(score.x));
 	text.setPosition(320 / 2, 0);
+	text.setFillColor(sf::Color::Red);
 	window->draw(text);
 	text.setString(std::to_string(score.y));
 	text.setPosition(320 / 2, 240 - 24);
+	text.setFillColor(sf::Color::Blue);
 	window->draw(text);
 	shape.setPosition(pos);
 	window->draw(shape);
